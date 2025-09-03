b9f4e592ba4b8f3e04f684d6082ffac55e4f42a4
Salvatore Sisay <sosofsisay@gmail.com>
2025-09-03 01:15:01 -0600

    Agregando funcion de nombres y validacion de boton

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo() {
+	const input = document.getElementById('amigo');
+	const nombre = input.value.trim();
+	const lista = document.getElementById('listaAmigos');
+	let error = document.getElementById('errorMensaje');
+	if (!error) {
+		error = document.createElement('li');
+		error.id = 'errorMensaje';
+		error.style.color = 'red';
+		error.style.listStyle = 'none';
+        error.style.fontFamily = 'Arial, sans-serif';
+		lista.parentNode.insertBefore(error, lista);
+	}
+	if (!nombre) {
+		error.textContent = 'Por favor, ingrese un nombre';
+		return;
+	} else {
+		error.textContent = '';
+	}
+	nombresAmigos.push(nombre);
+	input.value = '';
+	mostrarNombres();
+}
+
+function mostrarNombres() {
+	const lista = document.getElementById('listaAmigos');
+	lista.innerHTML = '';
+	nombresAmigos.forEach(function(nombre) {
+		const li = document.createElement('li');
+		li.textContent = nombre;
+		lista.appendChild(li);
+	});
+}
+
